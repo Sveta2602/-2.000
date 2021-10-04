cfdfd6ca5cead802905071abb3ddb93348e19062
Sveta2602 <91688580+Sveta2602@users.noreply.github.com>
2021-10-04 23:13:23 +0300

    Add files via upload

---
+++ lab02.01.cpp
@@ -0,0 +1,32 @@
+﻿
+
+// Lab_02.cpp
+//Яшина Світлана
+//Лабораторна робота №2.
+//Лінійні програми.
+//Варіант 24
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a;  // 
+	double z1;  // результат обчислення 1-го виразу
+	double z2;  // результат обчислення 2-го виразу
+	
+	cout << "a = "; cin >> a;
+
+
+		z1 = (sin(a) + sin(5 * a) - sin(3 * a)) / (cos(a) - cos(3 * a) + cos(5 * a));
+		z2 = sin(3 * a) / cos(3 * a);
+
+		cout << endl;
+		cout << "z1 = " << z1 << endl;
+		cout << "z2 = " << z2 << endl;
+
+		cin.get();
+		return 0;
+}
+
